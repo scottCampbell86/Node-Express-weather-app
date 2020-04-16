3affe64102e2d3fe158e9bc6b02762803d7019d1
Scott Campbell <stc9486@gmail.com>
2020-04-16 09:54:40 -0700

    temp

--- src/app.js
+++ src/app.js
@@ -1,44 +1,67 @@
 const path = require('path')
-//not just defining a const, but this is declaring a function with the label express, this loads express into our app, giving us access to the express function
 const express = require('express')
-//declaring a const with the label app and assigning it to the invocation of the express function
+const hbs = require('hbs')
+
 const app = express()
-//define paths for express config
-const viewsPath = path.join(__dirname, '../templates/views')
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const partialsPath = path.join(__dirname, '../templates/partials')
-//set up handlebars engine and views location
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/views/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-//set up static directory to serve
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Weather'
-  })
-})
-
-app.get('/help', (req, res) => {
-  res.render('help', {
-    text: 'This is the help message'
-  })
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Scott Campbell'
+    })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About Me',
-    name: 'Scott Campbell'
-  })
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
 })
 
 app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'Stormy',
-    location: 'Portland'
-  })
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Scott Campbell',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Scott Campbell',
+        errorMessage: 'Page not found.'
+    })
 })
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+    console.log('Server is up on port 3000.')
 })
