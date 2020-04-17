ef0dd18edf1fa9b6679121cb85abc8267e062cba
Scott Campbell <stc9486@gmail.com>
2020-04-16 20:59:35 -0700

    fetch from submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,26 @@
-console.log('Client side javascript file is loaded!')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+
+weatherForm.addEventListener('submit', (e) => {
+  console.log('click heard')
+    e.preventDefault()
+
+    const location = search.value
+    
+    messageOne.textContent = "Loading..."
+    messageOne.textContent = ""
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+              messageOne.textContent = data.location
+              messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
