3704629dba4a60d8074c9b1c5fdd73a58f0d21fb
Scott Campbell <stc9486@gmail.com>
2020-10-26 16:25:59 -0400

    units

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const request = require('request');
 
-const url = 'https://api.weatherstack.com/current?access_key=c2cb26c7e5a77c08bb51971d4efc236d&query=37.8267,-122.4233';
+const url = 'https://api.weatherstack.com/current?access_key=c2cb26c7e5a77c08bb51971d4efc236d&query=37.8267,-122.4233&units=f';
 
-request({ url: url}, (err, res) => {
-  const data = JSON.parse(res.body);
-  console.log(data);
-})
+request({ url: url, json: true }, (err, res) => {
+  let currTemp = res.body.current.temperature;
+
+  console.log(`The current Temperature is: ${currTemp} degrees`);
+});
 
